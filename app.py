a003e84a67adb7664fac696f2254a6631f02762d
Thomas Blomberg <thomasblomberg@MacBook-Pro-16.local>
2025-12-15 07:49:22 +0100

    Lade till en snygg HTML-design

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! Min första moderna deploy till Azure."
+    # Här säger vi åt Flask att rendera HTML-filen vi skapade
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
